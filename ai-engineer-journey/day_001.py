9820c62472ccf77a786e8ed1c1315e7a2b481021
Harish141296 <harishparamathmananda@gmail.com>
2026-05-01 08:43:36 +0530

    Day 1: continues top 3 without sorting

--- ai-engineer-journey/day_001.py
+++ ai-engineer-journey/day_001.py
@@ -8,6 +8,7 @@ class Solution:
         self.ls = ls 
         
     def method1(self)-> list[int]:
+        # with bubble sort
         print(self.ls) 
         top1, top2, top3 = 0, 0, 0 
         len_of_arr = len(self.ls)
@@ -32,11 +33,37 @@ class Solution:
         return (top1, top2, top3)
             
         
-    
+    def method2(self) -> list[int]:
+        # without sorting 
+        top1, top2, top3 = float('-inf'),float('-inf'),float('-inf')
+        len_of_arr = len(self.ls)
+        if len_of_arr and len_of_arr < 2 : 
+            top1 = self.ls[0]
+            return [top1,top2,top3]
+        if len(self.ls) < 3 : 
+            top1 = max(self.ls)
+            top2 = self.ls[0] if self.ls[0] != top1 else self.ls[1]
+            return  [top1,top2,top3]
 
-
+        max_count = 3
+        initializer = 0 
+        max_ls = [top1, top2, top3]
+        while initializer < max_count: 
+            maximum = float('-inf')
+            for i in range(len_of_arr):
+                if self.ls[i] > maximum:
+                    maximum = self.ls[i]
+            max_ls[initializer] = maximum
+            self.ls.remove(maximum)
+            initializer += 1
+            len_of_arr -= 1 
+            
+        return max_ls
+            
 if __name__ == '__main__':
     ls = [10,2,3,1,14,15,4,8]
     sol = Solution(ls)
     top_3:list[int] = sol.method1()
+    print(top_3)
+    top_3:list[int] = sol.method2()
     print(top_3)
