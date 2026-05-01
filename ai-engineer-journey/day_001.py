0bc63fae28c0c5bf64c2434b988d56248699fe64
Harish141296 <harishparamathmananda@gmail.com>
2026-05-01 07:29:40 +0530

    Day 1: AI Engineer Journey Begins.

---
+++ ai-engineer-journey/day_001.py
@@ -0,0 +1,42 @@
+"""
+write a function that takes a list of numbers and returns the top 3 highest values without using sort()
+"""
+
+class Solution:
+    def __init__(self, ls):
+        print("write a function that takes a list of numbers and returns the top 3 highest values without using sort()")
+        self.ls = ls 
+        
+    def method1(self)-> list[int]:
+        print(self.ls) 
+        top1, top2, top3 = 0, 0, 0 
+        len_of_arr = len(self.ls)
+        if len_of_arr and len_of_arr < 2 : 
+            top1 = self.ls[0]
+            return [top1,top2,top3]
+        if len(self.ls) < 3 : 
+            top1 = max(self.ls)
+            top2 = self.ls[0] if self.ls[0] != top1 else self.ls[1]
+            return  [top1,top2,top3]
+        
+        left, right = 0, len_of_arr -1 
+        while right > left:
+            for i in range(right):
+                if self.ls[i] > self.ls[i + 1]:
+                    self.ls[i], self.ls[i + 1] = self.ls[i + 1], self.ls[i]
+            right -= 1
+
+            print(self.ls) 
+        
+        top1,top2,top3 = self.ls[-1], self.ls[-2], self.ls[-3]
+        return (top1, top2, top3)
+            
+        
+    
+
+
+if __name__ == '__main__':
+    ls = [10,2,3,1,14,15,4,8]
+    sol = Solution(ls)
+    top_3:list[int] = sol.method1()
+    print(top_3)
